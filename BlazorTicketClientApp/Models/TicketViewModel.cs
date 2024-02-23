14826b03d033d225c2d52437cec2359ce03985e6
OttJoh94 <olg.johansson@gmail.com>
2024-02-23 10:30:46 +0100

    finalizing merging with master

--- BlazorTicketClientApp/Models/TicketViewModel.cs
+++ BlazorTicketClientApp/Models/TicketViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace BlazorTicketClientApp.Models
+﻿using Newtonsoft.Json;
+
+namespace BlazorTicketClientApp.Models
 {
 	public class TicketViewModel
 	{
 		public int Id { get; set; }
 		public string? Title { get; set; }
 		public string? Description { get; set; }
+		[JsonProperty("submittedBy")]
 		public string? SubtmittedBy { get; set; }
 		public bool? IsResolved { get; set; }
 		//Gjorde tag bara som en string i min TicketViewModel
